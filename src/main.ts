675c75efd0ea318b2e796c067b20abefb445c3fa
yupenglei <yupengniaosanshi@gmail.com>
2022-01-02 20:09:26 +0800

    类型声明从types文件移动到main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,44 @@ import { RoomDecorate } from "./role/room";
 import { watcher } from "./watch-client";
 import { workerFactory } from "./role/creeps";
 
+declare global {
+
+  type ROLE_HARVESTER = "harvester";
+  type ROLE_UPGRADER = "upgrader";
+  type ROLE_BUILDER = "builder";
+  type ROLE_REPAIRER = "repairer"
+  type ROLE_TRANSFERER = "transferer"
+  type CreepRole = ROLE_HARVESTER | ROLE_UPGRADER | ROLE_BUILDER | ROLE_REPAIRER | ROLE_TRANSFERER
+
+  interface CreepSpawnConfig {
+    role: CreepRole,
+    minCount: number,
+    body: BodyPartConstant[]
+  }
+
+// memory extension samples
+  interface CreepMemory {
+    role: CreepRole;
+    room?: string;
+    working?: boolean;
+  }
+
+  interface RoomMemory {
+    roleCount: { [role: string]: number };
+  }
+
+  interface Memory {
+    uuid: number;
+    log: any;
+  }
+
+// `global` extension samples
+  namespace NodeJS {
+    interface Global {
+      log: any;
+    }
+  }
+}
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
