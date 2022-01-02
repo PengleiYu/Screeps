fa3bfc12f83481c4ac813cb37ecb3e78dc28760f
yupenglei <yupengniaosanshi@gmail.com>
2022-01-02 20:37:51 +0800

    tower添加heal逻辑

--- src/role/tower.ts
+++ src/role/tower.ts
@@ -8,26 +8,35 @@ class TowerDecorator {
   run() {
     if (this.attack()) return;
     if (this.heal()) return;
+    if (this.repair()) return;
   }
 
   private heal(): boolean {
+    const closestCreep = this.tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+      filter: creep => creep.hits < creep.hitsMax
+    });
+    if (!closestCreep) return false;
+
+    this.tower.heal(closestCreep);
+    return true;
+  }
+
+  private repair(): boolean {
     const closestStructure = this.tower.pos.findClosestByRange(FIND_STRUCTURES, {
       filter: (structure) => structure.hits < structure.hitsMax
     });
-    if (closestStructure) {
-      this.tower.repair(closestStructure);
-      return true;
-    }
-    return false;
+    if (!closestStructure) return false;
+
+    this.tower.repair(closestStructure);
+    return true;
   }
 
   private attack(): boolean {
     const closestHostile = this.tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestHostile) {
-      this.tower.attack(closestHostile);
-      return true;
-    }
-    return false;
+    if (!closestHostile) return false;
+
+    this.tower.attack(closestHostile);
+    return true;
   }
 }
 
